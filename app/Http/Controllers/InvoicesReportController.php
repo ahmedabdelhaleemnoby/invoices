e40a79fb102a0aac8bf61c2c6c78f0b465dbf598
ahmedabdelahleem <90034764+ahmedabdelahleem@users.noreply.github.com>
2023-04-06 23:18:32 +0200

    0.1.0

--- app/Http/Controllers/InvoicesReportController.php
+++ app/Http/Controllers/InvoicesReportController.php
@@ -15,27 +15,18 @@ class InvoicesReportController extends Controller
 
     public function Search_invoices(Request $request)
     {
-
         $rdio = $request->rdio;
-
-
         // في حالة البحث بنوع الفاتورة
-
         if ($rdio == 1) {
-
-
             // في حالة عدم تحديد تاريخ
             if ($request->type && $request->start_at == '' && $request->end_at == '') {
-
                 $invoices = invoices::where('Status', '=', $request->type)->get();
                 return $invoices;
                 $type = $request->type;
                 return view('reports.invoices_report', compact('type'))->with('invoices', $invoices);
             }
-
             // في حالة تحديد تاريخ استحقاق
             else {
-
                 $start_at = date($request->start_at);
                 $end_at = date($request->end_at);
                 $type = $request->type;
@@ -43,9 +34,6 @@ class InvoicesReportController extends Controller
                 return view('reports.invoices_report', compact('type', 'start_at', 'end_at'))->with('invoices', $invoices);
             }
         }
-
-        //====================================================================
-
         // في البحث برقم الفاتورة
         else {
 
