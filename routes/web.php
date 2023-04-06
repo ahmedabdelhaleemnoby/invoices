498d869db5a9dabf1acbee5e09e74e2e78ec3312
ahmedabdelahleem <90034764+ahmedabdelahleem@users.noreply.github.com>
2023-04-06 23:19:28 +0200

    0.1.0

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [AdminController::class, 'home'])->middleware(['auth', 'verified']);
-// Route::get('/dashboard', [AdminController::class, 'home'])->middleware(['auth', 'verified']);
 // Route::get('/', function () {
 //     return view('index');
 // })->middleware(['auth', 'verified']);
